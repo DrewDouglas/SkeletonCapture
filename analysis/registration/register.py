ea888236690f3a08304055c7db912dfa779933b0
haniawni <hani.awni@gmail.com>
2017-03-09 13:34:41 -0600

    start register.py script

---
+++ analysis/registration/register.py
@@ -0,0 +1,12 @@
+# TODO CLI: inputs: skeleton CSV from, skeleton CSV onto, 'b' or 'e' trust start flag, destination_CSV name, transform_dest name
+
+# TODO read CSV from
+# TODO read CSV onto
+
+# TODO select canonical skeletons of associated times
+
+# TODO find mapping
+
+# TODO transform all of from into onto as from_onto
+
+# TODO output from_onto to CSV
